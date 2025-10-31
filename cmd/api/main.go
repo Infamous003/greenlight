83bc3e79e0b7de1963dfd162107ce1720bd2328f
Infamous003 <arfathsyed0312@gmail.com>
2025-10-31 15:21:18 +0530

    feat: set up initial project structure
    - Set up chi router and a simple server
    - Add a simple healthcheck handler

---
+++ cmd/api/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int    // port on which server is listening on
+	env  string // prod, dev, testing, etc
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+func main() {
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 9090, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	r := chi.NewRouter()
+	r.Get("/v1/healthcheck", app.healthcheckHandler)
+
+	s := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      r,
+		IdleTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("starting server", "addr", s.Addr, "env", cfg.env)
+
+	err := s.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
+}
