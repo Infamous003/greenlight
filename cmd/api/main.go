93d11ffbd55194ae9d21b041977fb32308609400
Infamous003 <arfathsyed0312@gmail.com>
2025-11-04 10:57:09 +0530

    feat(db): add postgres db connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
@@ -9,6 +11,7 @@ import (
 	"time"
 
 	"github.com/go-chi/chi/v5"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -16,6 +19,12 @@ const version = "1.0.0"
 type config struct {
 	port int    // port on which server is listening on
 	env  string // prod, dev, testing, etc
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
 }
 
 type application struct {
@@ -27,9 +36,27 @@ func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 9090, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	// reading the dsn value
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// creating a connection pool
+	db, err := openDB(&cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	logger.Info("database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -51,7 +78,36 @@ func main() {
 
 	logger.Info("starting server", "addr", s.Addr, "env", cfg.env)
 
-	err := s.ListenAndServe()
+	err = s.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(cfg *config) (*sql.DB, error) {
+	// Open creates an empty connection pool, using the driver name, and dsn string
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// max num of idle + in-use conns, val 0 or less is unlimited
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+
+	// max idle conns, is always <= Max open conns
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	// idle timeout for conns in pool
+	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// if the connecton couldn't be established successfully within 5s, then an error is raised
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
